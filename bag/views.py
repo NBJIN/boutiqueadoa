5e890a00ffa7c1c4e9b2ead1a4c3d6cf69ad959f
noreen <noreennelligancilms@gmail.com>
2023-03-31 09:24:16 +0000

    update views and product detail

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -11,7 +11,7 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product tot he shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('refirect_url')
+    redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -20,5 +20,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    # print(request.session['bag'])
     return redirect(redirect_url)
