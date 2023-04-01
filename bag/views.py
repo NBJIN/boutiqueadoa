9b50bd30645ebada20aa278e04989e49befbd4e9
noreen <noreennelligancilms@gmail.com>
2023-04-01 12:38:42 +0000

    look for error with size on products

--- bag/views.py
+++ bag/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect
 
+# Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag contents page  """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product tot he shopping bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None 
+    size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if size: 
+    if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
@@ -25,12 +25,12 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-    else: 
+    else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
 
     request.session['bag'] = bag
-    # print(request.session['bag'])
     return redirect(redirect_url)
+    
