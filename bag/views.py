d3ac021caddca6c0f122745c7ed2715bded4682f
noreen <noreennelligancilms@gmail.com>
2023-04-08 10:23:37 +0000

    Fix spacing in bag views

--- bag/views.py
+++ bag/views.py
@@ -5,12 +5,10 @@ from products.models import Product
 
 # Create your views here.
 
-
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -44,7 +42,7 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
@@ -102,3 +100,4 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+        
